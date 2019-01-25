f6cd2bf86e2ba2585a1378f2a58f6ffab0ed4478
Jalel YAKDI <jyakdi@e1r8p5.42.fr>
2019-01-25 17:36:13 +0100

    01

--- training/tools.py
+++ training/tools.py
@@ -10,12 +10,29 @@ def ft_train(data):
     rate = 0.5
     tmp0 = 0.0
     tmp1 = 0.0
+    m = len(data)
     #new tmp0 and tmp1
-    for line in data:
-        mile = int(line[0])
-        price = int(line[1])
-        #print("mile = {}, price = {}".format(mile, price))
-        diff = (tmp0 + tmp1 * mile) - price
-        print("diff = {}".format(diff))
+    sum0 = 0.0
+    sum1 = 0.0
+    for i in range(10):
+        sum0 = 0.0
+        sum1 = 0.0
+        for line in data:
+            mile = int(line[0])
+            price = int(line[1])
+            #print("mile = {}, price = {}".format(mile, price))
+            sum0 = sum0 + (tmp0 + tmp1 * mile) - price
+            sum1 = sum1 + tmp0 * mile
+            #sum1 = sum1 + (((tmp0 + tmp1 * mile) - price) * mile)
+            #print("diff = {}".format(diff0))
+            #print("diff = {}".format(diff1))
+        tmp0 = sum0 / (100 * m)
+        tmp1 = sum1 / (100 * m)
+        print("new tmp0 = {}".format(tmp0))
+        print("new tmp1 = {}".format(tmp1))
+        theta0 = tmp0
+        theta1 = tmp1
     theta0 = tmp0
     theta1 = tmp1
+    print("last value of theta0 = {}".format(theta0))
+    print("last value of theta1 = {}".format(theta1))
